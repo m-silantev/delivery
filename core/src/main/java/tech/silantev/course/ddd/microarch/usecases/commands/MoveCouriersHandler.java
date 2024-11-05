9285f8042e902b8708c49818321008e3a7121470
Maksim Silantev <m.silantev@crystals.ru>
2024-11-06 00:35:45 +0300

    add update operation after complete MoveCouriersCommand and add better exception handling (WIP)

--- core/src/main/java/tech/silantev/course/ddd/microarch/usecases/commands/MoveCouriersHandler.java
+++ core/src/main/java/tech/silantev/course/ddd/microarch/usecases/commands/MoveCouriersHandler.java
@@ -2,6 +2,7 @@ package tech.silantev.course.ddd.microarch.usecases.commands;
 
 import an.awesome.pipelinr.Command;
 import com.github.sviperll.result4j.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import tech.silantev.course.ddd.microarch.domain.courier.aggregate.Courier;
@@ -10,8 +11,8 @@ import tech.silantev.course.ddd.microarch.ports.CourierRepository;
 import tech.silantev.course.ddd.microarch.ports.OrderRepository;
 
 import java.util.List;
-import java.util.Optional;
 
+@Slf4j
 @Component
 public class MoveCouriersHandler implements Command.Handler<MoveCouriersCommand, Result<Void, String>> {
 
@@ -26,20 +27,49 @@ public class MoveCouriersHandler implements Command.Handler<MoveCouriersCommand,
 
     @Override
     public Result<Void, String> handle(MoveCouriersCommand command) {
-        List<Order> assignedOrders = orderRepository.getAllAssigned().throwError(RuntimeException::new);
-        assignedOrders.stream().filter(order -> order.getCourierId().isPresent()).forEach(order -> {
-            Optional<Courier> optCourier = courierRepository.getById(order.getCourierId().get()).discardError();
-            if (optCourier.isEmpty()) {
-                return;
+        Result<List<Order>, Exception> resultAllAssignedOrders = orderRepository.getAllAssigned();
+        if (resultAllAssignedOrders.isError()) {
+            return resultAllAssignedOrders.mapError(Exception::getMessage).map(orders -> null);
+        }
+        for (Order order : resultAllAssignedOrders.discardError().get()) {
+            if (order.getCourierId().isEmpty()) {
+                String message = "Order with ASSIGNED status and no courierId set was found, " + order;
+                log.warn(message);
+                return Result.error(message);
             }
-            Courier courier = optCourier.get();
+            Result<Courier, Exception> resultCourier = courierRepository.getById(order.getCourierId().get());
+            if (resultCourier.isError()) {
+                return resultCourier.mapError(exception -> {
+                    log.error(exception.getMessage());
+                    return exception.getMessage();
+                }).map(courier -> null);
+            }
+            Courier courier = resultCourier.discardError().get();
             if (courier.getDistanceTo(order.getLocation()) != 0) {
                 courier.makeOneStepTo(order.getLocation());
-                return;
+                Result<Courier, Exception> resultUpdateCourier = courierRepository.update(courier);
+                if (resultUpdateCourier.isError()) {
+                    return resultUpdateCourier.mapError(exception -> {
+                        log.error(exception.getMessage());
+                        return exception.getMessage();
+                    }).map(_courier -> null);
+                }
+                log.info("Courier {} made a step forward order location", courier);
+                continue;
             } // else complete order
-            order.complete().throwError(RuntimeException::new);
+            Result<Order, String> resultOfCompleteOrder = order.complete();
+            if (resultOfCompleteOrder.isError()) {
+                return resultOfCompleteOrder.mapError(message -> {
+                    log.error(message);
+                    return message;
+                }).map(_order -> null);
+            }
+            Result<Order, Exception> resultUpdate = orderRepository.update(order);
+            if (resultUpdate.isError()) {
+//                log.error();
+            }
             courier.setFree();
-        });
+        }
         return Result.success(null);
     }
 }
