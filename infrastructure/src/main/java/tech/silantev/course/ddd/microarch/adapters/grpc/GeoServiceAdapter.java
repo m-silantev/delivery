245619f3d26bdf0423aa34ed63033d8ddf3b86ed
Maksim Silantev <m.silantev@crystals.ru>
2024-11-08 00:35:34 +0300

    use grpc-client-spring-boot-starter

--- infrastructure/src/main/java/tech/silantev/course/ddd/microarch/adapters/grpc/GeoServiceAdapter.java
+++ infrastructure/src/main/java/tech/silantev/course/ddd/microarch/adapters/grpc/GeoServiceAdapter.java
@@ -1,36 +1,28 @@
 package tech.silantev.course.ddd.microarch.adapters.grpc;
 
 import com.github.sviperll.result4j.Result;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.StatusRuntimeException;
-import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 import tech.silantev.course.ddd.microarch.domain.sharedkernel.Location;
 import tech.silantev.course.ddd.microarch.ports.GeoService;
 
-@Slf4j
 @Service
 public class GeoServiceAdapter implements GeoService {
 
+    @GrpcClient("geo")
+    private GeoGrpc.GeoBlockingStub blockingStub;
+
     @Override
     public Result<Location, Exception> getLocation(String address) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 5004)
-                .usePlaintext()
-                .build();
-
-        GeoGrpc.GeoBlockingStub stub = GeoGrpc.newBlockingStub(channel);
-
-        GetGeolocationRequest request = GetGeolocationRequest.newBuilder().setStreet(address).build();
-        GetGeolocationReply response;
         try {
-            // Call the original method on the server.
-            response = stub.getGeolocation(request);
-            log.info("response {}", response);
-        } catch (StatusRuntimeException e) {
-            // Log a warning if the RPC fails.
+            GetGeolocationRequest request = GetGeolocationRequest.newBuilder()
+                    .setStreet(address)
+                    .build();
+            GetGeolocationReply response = blockingStub.getGeolocation(request);
+            Location location = Location.create(response.getLocation().getX(), response.getLocation().getY());
+            return Result.success(location);
+        } catch (Exception e) {
+            return Result.error(e);
         }
-
-        return null;
     }
 }
